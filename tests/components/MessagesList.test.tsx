8421c2a9ec77f7cfc95a558ee6c59bac728d35a0
Ihor Marusyk <ih2502mk@gmail.com>
2020-11-09 10:52:01 -0500

    updated tests

--- tests/components/MessagesList.test.tsx
+++ tests/components/MessagesList.test.tsx
@@ -3,11 +3,13 @@ import renderer from "react-test-renderer";
 import { MessagesList } from "../../public/components/MessagesList";
 import Message from "../../shared/Message";
 
+const testDate = new Date(Date.UTC(2020, 11, 9, 10, 10, 10, 10));
+
 const mockMessages: Message[] = [
-    { id: "1", content: "Test Msg 1", author: "John", channelId: "ch1"},
-    { id: "2", content: "Test Msg 2", author: "Bob", channelId: "ch1"},
-    { id: "3", content: "Test Msg 3", author: "Alice", channelId: "ch1"},
-    { id: "4", content: "Test Msg 4", author: "Walter", channelId: "ch1"},
+    { id: "1", content: "Test Msg 1", author: "John", channelId: "ch1", created: testDate},
+    { id: "2", content: "Test Msg 2", author: "Bob", channelId: "ch1", created: testDate},
+    { id: "3", content: "Test Msg 3", author: "Alice", channelId: "ch1", created: testDate},
+    { id: "4", content: "Test Msg 4", author: "Walter", channelId: "ch1", created: testDate},
 ];
 
 describe("snapshots", () => {
